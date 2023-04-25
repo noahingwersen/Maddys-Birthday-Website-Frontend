67ae0f79311fee4acb5edc10632551f344953637
noahingwersen <noah.ingwersen1@gmail.com>
2023-04-24 21:16:15 -0400

    Updated ImageUpload

--- maddys-birthday-website/src/components/ImageUpload.jsx
+++ maddys-birthday-website/src/components/ImageUpload.jsx
@@ -20,38 +20,38 @@ const ImageUpload = ({ uploadFile }) => {
   }
 
   return (
-    <div
-      className="mb-2 flex flex-col items-center justify-center bg-gray-200 min-h-[250px] rounded-md border-2 border-gray-500 border-dashed"
+    <label
+      forHtml="uploadImage"
+      className="mb-2 flex flex-col items-center justify-center bg-gray-300 min-h-[250px] rounded-md border-2 border-gray-500 border-dashed hover:bg-gray-200 hover:cursor-pointer"
       onDragOver={onDragEvent}
       onDrop={onDropEvent}
     >
-      <label
-        htmlFor="uploadImage"
-        className=" hover:cursor-pointer mb-2 border-2 border-green-500 hover:bg-green-500 text-green-500 hover:text-white p-2 rounded-md"
+      <svg
+        aria-hidden="true"
+        class="w-10 h-10 mb-3 text-gray-400"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        {' '}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-arrow-up"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
-          />
-        </svg>
-      </label>
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+        ></path>
+      </svg>
+      <p class="mb-2 text-sm text-gray-500">
+        <span class="font-semibold">Click to upload</span> or drag and drop
+      </p>
+      <p class="text-xs text-gray-500">SVG, PNG, JPG or GIF</p>
       <input
         className="hidden"
         type="file"
         id="uploadImage"
         onChange={onFileInput}
       />
-      <h1>Drag images to upload</h1>
-    </div>
+    </label>
   )
 }
 export default ImageUpload
