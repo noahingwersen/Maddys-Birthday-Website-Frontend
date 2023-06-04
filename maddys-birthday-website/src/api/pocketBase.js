d77ef59053401b26b66a7e89595cd65caeaf66e9
noahingwersen <noah.ingwersen1@gmail.com>
2023-06-04 11:19:12 -0400

    Updated backend URL

--- maddys-birthday-website/src/api/pocketBase.js
+++ maddys-birthday-website/src/api/pocketBase.js
@@ -1,5 +1,3 @@
 import PocketBase from 'pocketbase'
 
-export const pb = new PocketBase(
-  'https://m-and-n.duckdns.org/maddys-birthday-backend/'
-)
+export const pb = new PocketBase('https://maddys-birthday-backend.duckdns.org/')
