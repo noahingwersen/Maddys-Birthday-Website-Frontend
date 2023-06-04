dd607d003cc90a4eb9ec0d17785a6f91bd8f4f40
noahingwersen <noah.ingwersen1@gmail.com>
2023-06-03 20:40:23 -0400

    Updated footers

--- maddys-birthday-website/src/pages/about/components/AboutFooter.jsx
+++ maddys-birthday-website/src/pages/about/components/AboutFooter.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 const AboutFooter = () => {
   return (
     <div className="text-center text-sm mt-2 mb-1">
-      <p>&copy; 2023</p>
       <Link className="hover:underline" to="/">
         Home
       </Link>
+      <p>&copy; 2023</p>
     </div>
   )
 }
--- maddys-birthday-website/src/pages/home/components/HomeFooter.jsx
+++ maddys-birthday-website/src/pages/home/components/HomeFooter.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 const HomeFooter = () => {
   return (
     <div className="text-center text-sm mt-2 mb-1">
-      <p>&copy; 2023</p>
       <Link className="hover:underline" to="/about">
-        About
+        How does this work?
       </Link>
+      <p>&copy; 2023</p>
     </div>
   )
 }
