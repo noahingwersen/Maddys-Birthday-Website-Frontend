7076a0238cf0eaa81eb4c2733f829253e0ec3177
noahingwersen <noah.ingwersen1@gmail.com>
2023-06-29 13:59:17 -0400

    Updated preview and icon

--- maddys-birthday-website/src/components/Navbar.jsx
+++ maddys-birthday-website/src/components/Navbar.jsx
@@ -55,13 +55,11 @@ const Navbar = () => {
             <div className="flex flex-shrink-0 items-center">
               <img
                 className="block h-8 w-auto lg:hidden"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
+                src="/cake.svg"
               />
               <img
                 className="hidden h-8 w-auto lg:block"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
+                src="/cake.svg"
               />
             </div>
             <div className="hidden sm:ml-6 sm:block">
