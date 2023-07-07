e00fc458f19e04d8dbf8bd67cc78308be5e2550a
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-07 13:54:53 -0400

    Added feed footer

--- maddys-birthday-website/src/pages/feed/FeedPage.jsx
+++ maddys-birthday-website/src/pages/feed/FeedPage.jsx
@@ -6,6 +6,7 @@ const FeedPage = () => {
     <div className="h-full w-full flex flex-col items-center overflow-y-auto bg-blue-400">
       <FeedHeader />
       <Feed />
+      <p className="mb-5">No more posts to show</p>
     </div>
   )
 }
