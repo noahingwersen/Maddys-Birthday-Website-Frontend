cdcb94f1872bb66ebdd2b6cd539361bd3c87f5e3
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-07 14:10:04 -0400

    Updated footer

--- maddys-birthday-website/src/pages/feed/FeedPage.jsx
+++ maddys-birthday-website/src/pages/feed/FeedPage.jsx
@@ -1,4 +1,5 @@
 import Feed from "./components/Feed"
+import FeedFooter from "./components/FeedFooter"
 import FeedHeader from "./components/FeedHeader"
 
 const FeedPage = () => {
@@ -6,7 +7,7 @@ const FeedPage = () => {
     <div className="h-full w-full flex flex-col items-center overflow-y-auto bg-gray-100 sm:bg-blue-400">
       <FeedHeader />
       <Feed />
-      <p className="my-5">No more posts to show</p>
+      <FeedFooter />
     </div>
   )
 }
---
+++ maddys-birthday-website/src/pages/feed/components/FeedFooter.jsx
@@ -0,0 +1,6 @@
+const FeedFooter = () => {
+  return (
+    <p className="mb-5 mt-5 sm:mt-0">No more posts to show</p>
+  )
+}
+export default FeedFooter
