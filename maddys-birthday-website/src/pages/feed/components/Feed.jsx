d62e5cb7665fbabe85903ab955f97b8c0296c6ec
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-07 13:38:43 -0400

    Updated feed to posts

--- maddys-birthday-website/src/pages/feed/components/Feed.jsx
+++ maddys-birthday-website/src/pages/feed/components/Feed.jsx
@@ -1,23 +1,46 @@
 import useApiData from "../../../hooks/useApiData"
 import { pb } from "../../../api/pocketBase"
 import Post from "./Post"
+import { useEffect, useState } from "react"
 
 const Feed = () => {
     const [messagesLoading, messages, messageErrors] = useApiData('messages', {sort: 'user', expand: 'user'})
     const [picturesLoading, pictures, pictureErrors] = useApiData('pictures', {sort: 'user', expand: 'user'})
 
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+      if (messages && pictures) {
+        const newPosts = messages.concat(pictures)
+
+        newPosts.sort((a, b) => {
+          const dateA = new Date(a.updated)
+          const dateB = new Date(b.updated)
+
+          return dateA-dateB
+        })
+
+        setPosts(newPosts)
+      }
+    
+    }, [messages, pictures])
+    
 
   return (
     <div>
-        {pictures && pictures.map((picture) => {
-          const content = <img className="w-full object-contain" src={pb.getFileUrl(picture, picture.image)}/> 
-          return <Post key={picture.id} user={picture.expand.user} time={new Date(picture.updated)} content={content}/>
-        })}
+      {posts && posts.map((post) => {
+        let content
+        if (post.collectionName == 'messages') {
+          content = <p>{post.text}</p>
+        } else if (post.collectionName == 'pictures') {
+          content = <img className="w-full object-contain" src={pb.getFileUrl(post, post.image)}/>
+        } else {
+          console.log(`Unknown collection: ${post.collectionName}`)
+          return
+        }
 
-        {messages && messages.map((message) => {
-          const content = <p>{message.text}</p>
-          return <Post key={message.id} user={message.expand.user} time={new Date(message.updated)} content={content}/>
-        })}
+        return <Post key={post.id} user={post.expand.user} time={new Date(post.updated)} content={content}/>
+      })}
     </div>
   )
 }
