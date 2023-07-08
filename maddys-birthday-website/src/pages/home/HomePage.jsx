f69b340a0953d14ba8eea46381728ee41d51ec6a
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-07 20:54:50 -0400

    Added confetti

--- maddys-birthday-website/src/pages/home/HomePage.jsx
+++ maddys-birthday-website/src/pages/home/HomePage.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react'
 import CoundownMessage from './components/CoundownMessage'
+import Confetti from '../../components/Confetti'
 
 const HomePage = () => {
+  const [showConfetti, setShowConfetti] = useState(false)
+
+  useEffect(() => {
+    const maddysBirthday = new Date('2023-07-10T00:00:00.000')
+    const now = new Date()
+
+    if (maddysBirthday - now <=0) {
+      setShowConfetti(true)
+    }
+  
+  }, [])
+
   return (
     <div className="flex items-center h-full justify-center">
+      {showConfetti && <Confetti />}
       <CoundownMessage />
     </div>
   )
