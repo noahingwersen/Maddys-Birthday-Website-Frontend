9189301826587b0d0f620bd59b25da48b5059b88
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-09 23:08:20 -0400

    Updated profile stats
    Make sure we're only counting user submitted posts and messages

--- maddys-birthday-website/src/pages/profile/components/ProfileStats.jsx
+++ maddys-birthday-website/src/pages/profile/components/ProfileStats.jsx
@@ -1,14 +1,26 @@
-import useApiData from "../../../hooks/useApiData"
+import useApiData from '../../../hooks/useApiData'
+import useAuth from '../../../hooks/useAuth'
 
 const ProfileStats = () => {
-  const [messageLoading, messages, messageErrors] = useApiData('messages')
-  const [pictureLoading, pictures, pictureErrors] = useApiData('pictures')
+  const { user } = useAuth()
+  const [messageLoading, messages, messageErrors] = useApiData('messages', {
+    filter: `user='${user.id}'`,
+  })
+  const [pictureLoading, pictures, pictureErrors] = useApiData('pictures', {
+    filter: `user='${user.id}'`,
+  })
 
   return (
     <div className="p-2 m-2">
       <h1 className="text-3xl font-bold">Contributions</h1>
-      <h3>Total messages submitted: {messageLoading ? "loading..." : messages && messages.length}</h3>
-      <h3>Total pictures uploaded: {pictureLoading ? "loading..." : pictures && pictures.length}</h3>
+      <h3>
+        Total messages submitted:{' '}
+        {messageLoading ? 'loading...' : messages && messages.length}
+      </h3>
+      <h3>
+        Total pictures uploaded:{' '}
+        {pictureLoading ? 'loading...' : pictures && pictures.length}
+      </h3>
     </div>
   )
 }
