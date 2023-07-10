3a55c1f32c17ef47e40ff238886a70be31e97894
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-09 22:57:28 -0400

    Added default profile pic

--- maddys-birthday-website/src/pages/feed/components/Post.jsx
+++ maddys-birthday-website/src/pages/feed/components/Post.jsx
@@ -1,21 +1,34 @@
-import { pb } from "../../../api/pocketBase"
+import { pb } from '../../../api/pocketBase'
 
-const Message = ({user, time, content}) => {
-    const userAvatar = pb.getFileUrl(user, user.avatar)
-    const dateOptions = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'}
-    const timestamp = time.toLocaleDateString('en-US', dateOptions)
-    
+const Message = ({ user, time, content }) => {
+  let userAvatar
+  if (user.avatar) {
+    userAvatar = pb.getFileUrl(user, user.avatar)
+  } else {
+    userAvatar =
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+  }
+  const dateOptions = {
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  }
+  const timestamp = time.toLocaleDateString('en-US', dateOptions)
 
   return (
     <div className="sm:bg-gray-200 flex flex-col max-w-[450px] p-4 border sm:border-none sm:my-8 sm:rounded-md sm:w-[450px] w-full">
-        <div className="flex flex-row mb-4">
-            <img className="h-20 w-20 rounded-full object-cover me-4" src={userAvatar}/>
-            <div className="flex flex-col">
-                <h1 className="font-semibold text-2xl">{user.name}</h1>
-                <p className="text-gray-500">{timestamp}</p>
-            </div>
+      <div className="flex flex-row mb-4">
+        <img
+          className="h-20 w-20 rounded-full object-cover me-4"
+          src={userAvatar}
+        />
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-2xl">{user.name}</h1>
+          <p className="text-gray-500">{timestamp}</p>
         </div>
-        {content}
+      </div>
+      {content}
     </div>
   )
 }
